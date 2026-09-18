88929520459f0cd2392e2c0fc57870aa487205f8
linghutianhao <linghutianhao123>
2026-09-18 17:00:30 +0800

    增加代码块shell展示 , 优化注释文字

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -137,7 +137,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
-        additionalLanguages: ['java'],
+        additionalLanguages: ['java','bash'],
       },
     }),
 };
